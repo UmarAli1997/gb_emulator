093315cceacc8b4855447dbf7b501545422b359c
Umar Ali <umarali1997@gmail.com>
2023-02-13 22:09:33 +0000

    added logic to write boot rom into memory

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
+use std::path::Path;
+use std::fs;
+
+use crate::cpu::CPU;
+use crate::memory_bus::MemoryBus;
+
 
 mod memory_bus;
 mod cpu;
 
-pub struct CPU {
-    register: cpu::Registers,
-    pc: u16,
-    bus: memory_bus::MemoryBus
-}
-
-
 
 fn main() {
-    println!("Hello, world!");
+
+    let mut cpu = CPU::new();
+    let mut memory = MemoryBus::new();
+
+    let boot_path = Path::new("./BOOT/dmg_boot.bin");
+    let boot_rom = fs::read(boot_path).expect("File not found!");
+
+    for val in boot_rom.iter() {
+        //println!("{:#X}", val)
+        memory.copy_rom(0, &boot_rom);
+    }
+
+//     for i in 0..0x0102 {
+//         println!("{:#X}", memory.read_byte(i));
+//     }
 }
