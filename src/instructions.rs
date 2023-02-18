43b2a4a2f6b777938150602c9ef060e3922b183f
Umar Ali <umarali1997@gmail.com>
2023-02-18 17:25:11 +0000

    Adding more instructions

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,10 +1,27 @@
-pub enum Instructions{
+pub enum Instruction {
     // Load to the 8-bit register r, the data from the 8-bit register r'
-    LD_r_r(u8, u8),
+    LD_r_r{ r1: u8, r2: u8 },
+
     // Load to the 8-bit register r, the immediate data n
-    LD_r_n(u8),
+    LD_r_n{ r1: u8 },
+
     // Load to the 8-bit register r, data from the absolute address specified by the 16-bit register HL
-    LD_r_HL(u8, u16)
+    LD_r_HL{ r1: u8, r2: u16 },
+
+    // Load to the absolute address specified by the 16-bit register HL, data from the 8-bit register r
+    LD_HL_r { r1: u16, r2: u8 },
+
+    // Load to the absolute address specified by the 16-bit register HL, the immediate data n
+    LD_HL_n { r1: u16, r2: u8 },
+
+    // Load to the 8-bit A register, data from the absolute address specified by the 16-bit register BC
+    LD_A_BC { r1: u8, r2: u16 },
+
+    // Load to the 8-bit A register, data from the absolute address specified by the 16-bit register DE
+    LD_A_DE { r1:u8, r2: u16 },
+
+    // Load to the absolute address specified by the 16-bit register BC, data from the 8-bit A register
+    LD_BC_A { r1: u16, r2: u8 }
 
 }
 
