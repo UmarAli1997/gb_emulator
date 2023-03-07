ea489d0e993a5f190d7a6e894757450fcea95912
Umar Ali <umarali1997@gmail.com>
2023-03-07 01:10:35 +0000

    Changed code to accomodate for gameboy.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
 use std::path::Path;
 use std::fs;
 
-use cpu::{CPU, Registers, RegisterU8};
-use instructions::Instruction;
-use mmu::MemoryBus;
-
 mod mmu;
 mod cpu;
 mod instructions;
+mod gameboy;
+
+use cpu::{CPU, Registers, RegisterU8};
+use instructions::Instruction;
+use mmu::MemoryBus;
+use crate::gameboy::Gameboy;
+
+
 
 fn main() {
 
-    let mut cpu = CPU::new();
-    let mut memory = MemoryBus::new();
+    let mut gameboy = Gameboy {
+        cpu: CPU::new(),
+        memory: MemoryBus::new()
+    };
 
     let boot_path = Path::new("./BOOT/dmg_boot.bin");
     let boot_rom = fs::read(boot_path).expect("File not found!");
 
-    memory.copy_to_ram(0, &boot_rom);
+    gameboy.memory.copy_to_ram(0, &boot_rom);
 
     // for val in boot_rom.iter() {
     //     //println!("{:#X}", val)
@@ -26,9 +32,9 @@ fn main() {
 
     // Checking if the data has been loaded in correctly
     // for i in 0..0x0100 {
-    //     println!("{:#X}", memory.read_byte(i));
+    //     println!("{:#X}", gameboy.memory.read_byte(i));
     // }
 
-    cpu.execute();
+    gameboy.fetch();
 
 }
