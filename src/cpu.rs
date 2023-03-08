9a263c3ae832f5fd6d984fdf251b3f4af44cbdd4
Umar Ali <umarali1997@gmail.com>
2023-03-08 00:31:45 +0000

    Removed unused code

--- src/cpu.rs
+++ src/cpu.rs
@@ -129,42 +129,4 @@ impl Registers {
         }
     }
 
-    pub fn read_af(&self) -> u16 {
-        // Left shift register a by 8 bits and do a bitwise OR operation with register f
-        return (self.a as u16) << 8 | self.f as u16;
-    }
-
-    pub fn set_af(&mut self, value: u16) {
-        // Bitwise AND the MSB then right shift the result into the LSB to store into the register
-        self.a = ((value & 0xFF00) >> 8) as u8;
-        // No need to right shift the result here as the LSB is already in the correct position
-        self.f = (value & 0xFF) as u8;
-    }
-
-    pub fn read_bc(&self) -> u16 {
-        return (self.b as u16) << 8 | self.c as u16;
-    }
-
-    pub fn set_bc(&mut self, value: u16) {
-        self.b = ((value & 0xFF00) >> 8) as u8;
-        self.c = (value & 0xFF) as u8;
-    }
-
-    pub fn read_de(&self) -> u16 {
-        return (self.d as u16) << 8 | self.e as u16;
-    }
-
-    pub fn set_de(&mut self, value: u16) {
-        self.d = ((value & 0xFF00) >> 8) as u8;
-        self.e = (value & 0xFF) as u8;
-    }
-
-    pub fn read_hl(&self) -> u16 {
-        return (self.h as u16) << 8 | self.l as u16;
-    }
-
-    pub fn set_hl(&mut self, value: u16) {
-        self.h = ((value & 0xFF00) >> 8) as u8;
-        self.l = (value & 0xFF) as u8;
-    }
 }
