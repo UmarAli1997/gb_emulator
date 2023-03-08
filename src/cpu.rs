17c734c79cc8069715440cfbc8516034473394c4
Umar Ali <umarali1997@gmail.com>
2023-03-08 00:31:07 +0000

    Fixed bug with RegisterU16 enum not matching properly

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,7 +1,6 @@
 use std::convert::From;
 use crate::Gameboy;
 use crate::mmu::MemoryBus;
-use crate::instructions::Instruction;
 
 const ZERO_FLAG_BYTE_POSITION: u8 = 7;
 const SUBTRACT_FLAG_BYTE_POSITION: u8 = 6;
@@ -13,7 +12,7 @@ pub struct CPU {
     pub register: Registers,
     pub pc: u16,
     pub sp: u16,
-    flags: FlagsRegister
+    pub flags: FlagsRegister
 }
 
 // Initialising CPU with zero values
@@ -36,31 +35,31 @@ impl CPU {
         };
         cpu
     }
-
 }
 
-struct FlagsRegister {
+pub struct FlagsRegister {
     // Zero flag
-    z: bool,
+    pub z: bool,
     // Add/Subtract flag
-    n: bool,
+    pub n: bool,
     // Half-carry flag
-    h: bool,
+    pub h: bool,
     // Carry flag
-    c: bool
+    pub c: bool
 }
 
 pub struct Registers {
-    a: u8,
-    b: u8,
-    c: u8,
-    d: u8,
-    e: u8,
-    f: u8,
-    h: u8,
-    l: u8,
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub f: u8,
+    pub h: u8,
+    pub l: u8,
 }
 
+#[derive(Copy, Clone)]
 pub enum RegisterU8 {
     A,
     B,
@@ -68,12 +67,12 @@ pub enum RegisterU8 {
     D,
     E,
     F,
-    G,
     H,
     L
 }
 
-enum RegisterU16 {
+#[derive(Copy, Clone)]
+pub enum RegisterU16 {
     AF,
     BC,
     DE,
@@ -108,24 +107,24 @@ impl Registers {
 
     pub fn write_u8(&mut self, reg: RegisterU8, val: u8) {
         match reg {
-            A => self.a = val,
-            B => self.b = val,
-            C => self.c = val,
-            D => self.d = val,
-            E => self.e = val,
-            F => self.f = val, // This needs to be masked to the correct flag
-            H => self.h = val,
-            L => self.l = val,
+            RegisterU8::A => self.a = val,
+            RegisterU8::B => self.b = val,
+            RegisterU8::C => self.c = val,
+            RegisterU8::D => self.d = val,
+            RegisterU8::E => self.e = val,
+            RegisterU8::F => self.f = val, // This needs to be masked to the correct flag
+            RegisterU8::H => self.h = val,
+            RegisterU8::L => self.l = val,
             _ => panic!("Invalid write_u8")
         }
     }
 
     pub fn read_u16(&self, reg: RegisterU16) -> u16 {
         match reg {
-            AF => (self.a as u16) << 8 | self.f as u16,
-            BC => (self.b as u16) << 8 | self.c as u16,
-            DE => (self.d as u16) << 8 | self.e as u16,
-            HL => (self.h as u16) << 8 | self.l as u16,
+            RegisterU16::AF => (self.a as u16) << 8 | self.f as u16,
+            RegisterU16::BC => (self.b as u16) << 8 | self.c as u16,
+            RegisterU16::DE => (self.d as u16) << 8 | self.e as u16,
+            RegisterU16::HL => (self.h as u16) << 8 | self.l as u16,
             _ => panic!("Invalid read_u16")
         }
     }
