8a578e15100465b8bf5b14b62b89e6c50cd9b83e
Umar Ali <umarali1997@gmail.com>
2023-03-12 00:44:43 +0000

    Added write_u16 and u16_to_u8 functions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,4 @@
-use std::convert::From;
-use crate::Gameboy;
-use crate::mmu::MemoryBus;
+use std::{convert::From};
 
 const ZERO_FLAG_BYTE_POSITION: u8 = 7;
 const SUBTRACT_FLAG_BYTE_POSITION: u8 = 6;
@@ -10,8 +8,6 @@ const CARRY_FLAG_BYTE_POSITION: u8 = 4;
 // Building CPU up
 pub struct CPU {
     pub register: Registers,
-    pub pc: u16,
-    pub sp: u16,
     pub flags: FlagsRegister
 }
 
@@ -28,9 +24,9 @@ impl CPU {
                 e: 0,
                 h: 0,
                 l: 0,
+                pc: 0,
+                sp: 0,
             },
-            pc: 0,
-            sp: 0,
             flags: FlagsRegister { z: false, n: false, h: false, c: false }
         };
         cpu
@@ -49,14 +45,16 @@ pub struct FlagsRegister {
 }
 
 pub struct Registers {
-    pub a: u8,
-    pub b: u8,
-    pub c: u8,
-    pub d: u8,
-    pub e: u8,
-    pub f: u8,
-    pub h: u8,
-    pub l: u8,
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+    h: u8,
+    l: u8,
+    pub pc: u16,
+    pub sp: u16
 }
 
 #[derive(Copy, Clone)]
@@ -76,7 +74,9 @@ pub enum RegisterU16 {
     AF,
     BC,
     DE,
-    HL
+    HL,
+    PC,
+    SP
 }
 
 // If the register needs to be accessed as a u8 this will convert the bool in the struct to a u8
@@ -125,8 +125,50 @@ impl Registers {
             RegisterU16::BC => (self.b as u16) << 8 | self.c as u16,
             RegisterU16::DE => (self.d as u16) << 8 | self.e as u16,
             RegisterU16::HL => (self.h as u16) << 8 | self.l as u16,
+            RegisterU16::PC => self.pc,
+            RegisterU16::SP => self.sp,
             _ => panic!("Invalid read_u16")
         }
     }
+
+    pub fn write_u16(&mut self, reg: RegisterU16, val: u16) {
+        match reg {
+            RegisterU16::AF => {
+                let two_bytes = u16_to_u8(val);
+                self.write_u8(RegisterU8::A, two_bytes[0]);
+                self.write_u8(RegisterU8::F, two_bytes[1]);
+            }
+
+            RegisterU16::BC => {
+                let two_bytes = u16_to_u8(val);
+                self.write_u8(RegisterU8::B, two_bytes[0]);
+                self.write_u8(RegisterU8::C, two_bytes[1]);
+            }
+
+            RegisterU16::DE => {
+                let two_bytes = u16_to_u8(val);
+                self.write_u8(RegisterU8::D, two_bytes[0]);
+                self.write_u8(RegisterU8::E, two_bytes[1]);
+            }
+
+            RegisterU16::HL => {
+                let two_bytes = u16_to_u8(val);
+                self.write_u8(RegisterU8::H, two_bytes[0]);
+                self.write_u8(RegisterU8::L, two_bytes[1]);
+            }
+
+            RegisterU16::PC => self.pc = val,
+            RegisterU16::SP => self.sp = val,
+            _ => panic!("Invalid read_u16")
+        }
+
+        fn u16_to_u8(val: u16) -> [u8; 2] {
+            let high_byte = (val >> 8) as u8;
+            let low_byte = (val & 0xFF) as u8;
+            return [high_byte, low_byte];
+        }
+
+    }
+
 
 }
