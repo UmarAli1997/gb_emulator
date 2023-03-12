b9ed2b5529e297b521ebff5d327b2bda653c9382
Umar Ali <umarali1997@gmail.com>
2023-03-12 17:18:40 +0000

    Started on 0xCB prefix codes and implemented bit_r

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -91,7 +91,7 @@ impl Gameboy {
             // 0x3 opcodes
             0x30 => todo!(),
             0x31 => self.ld_rr_nn(RegisterU16::SP),
-            0x32 => todo!(),
+            0x32 => self.ld_hl_minus_a(),
             0x33 => todo!(),
             0x34 => todo!(),
             0x35 => todo!(),
@@ -196,12 +196,127 @@ impl Gameboy {
             0xAE => self.xor_hl(),
             0xAF => self.xor(RegisterU8::A),
 
+            //0xB opcodes
+            0xB0 => todo!(),
+            0xB1 => todo!(),
+            0xB2 => todo!(),
+            0xB3 => todo!(),
+            0xB4 => todo!(),
+            0xB5 => todo!(),
+            0xB6 => todo!(),
+            0xB7 => todo!(),
+            0xB8 => todo!(),
+            0xB9 => todo!(),
+            0xBA => todo!(),
+            0xBB => todo!(),
+            0xBC => todo!(),
+            0xBD => todo!(),
+            0xBE => todo!(),
+            0xBF => todo!(),
+
+            //0xC opcodes
+            0xC0 => todo!(),
+            0xC1 => todo!(),
+            0xC2 => todo!(),
+            0xC3 => todo!(),
+            0xC4 => todo!(),
+            0xC5 => todo!(),
+            0xC6 => todo!(),
+            0xC7 => todo!(),
+            0xC8 => todo!(),
+            0xC9 => todo!(),
+            0xCA => todo!(),
+            0xCB => self.cb_prefix(),
+            0xCC => todo!(),
+            0xCD => todo!(),
+            0xCE => todo!(),
+            0xCF => todo!(),
 
 
             _ => panic!("Opcode not implemented: {:#X}", opcode),
         }
     }
 
+fn cb_prefix(&mut self) {
+    let cb_code = self.read_instruction(self.cpu.register.pc);
+    self.cpu.register.pc += 1;
+
+    match cb_code {
+        // 0x4 codes
+        0x40 => self.bit_r(RegisterU8::B, 0),
+        0x41 => self.bit_r(RegisterU8::C, 0),
+        0x42 => self.bit_r(RegisterU8::D, 0),
+        0x43 => self.bit_r(RegisterU8::E, 0),
+        0x44 => self.bit_r(RegisterU8::H, 0),
+        0x45 => self.bit_r(RegisterU8::L, 0),
+        0x46 => todo!(),
+        0x47 => self.bit_r(RegisterU8::A, 0),
+        0x48 => self.bit_r(RegisterU8::B, 1),
+        0x49 => self.bit_r(RegisterU8::C, 1),
+        0x4A => self.bit_r(RegisterU8::D, 1),
+        0x4B => self.bit_r(RegisterU8::E, 1),
+        0x4C => self.bit_r(RegisterU8::H, 1),
+        0x4D => self.bit_r(RegisterU8::L, 1),
+        0x4E => todo!(),
+        0x4F => self.bit_r(RegisterU8::A, 1),
+
+        // 0x5 codes
+        0x50 => self.bit_r(RegisterU8::B, 2),
+        0x51 => self.bit_r(RegisterU8::C, 2),
+        0x52 => self.bit_r(RegisterU8::D, 2),
+        0x53 => self.bit_r(RegisterU8::E, 2),
+        0x54 => self.bit_r(RegisterU8::H, 2),
+        0x55 => self.bit_r(RegisterU8::L, 2),
+        0x56 => todo!(),
+        0x57 => self.bit_r(RegisterU8::A, 2),
+        0x58 => self.bit_r(RegisterU8::B, 3),
+        0x59 => self.bit_r(RegisterU8::C, 3),
+        0x5A => self.bit_r(RegisterU8::D, 3),
+        0x5B => self.bit_r(RegisterU8::E, 3),
+        0x5C => self.bit_r(RegisterU8::H, 3),
+        0x5D => self.bit_r(RegisterU8::L, 3),
+        0x5E => todo!(),
+        0x5F => self.bit_r(RegisterU8::A, 3),
+
+        // 0x6 codes
+        0x60 => self.bit_r(RegisterU8::B, 4),
+        0x61 => self.bit_r(RegisterU8::C, 4),
+        0x62 => self.bit_r(RegisterU8::D, 4),
+        0x63 => self.bit_r(RegisterU8::E, 4),
+        0x64 => self.bit_r(RegisterU8::H, 4),
+        0x65 => self.bit_r(RegisterU8::L, 4),
+        0x66 => todo!(),
+        0x67 => self.bit_r(RegisterU8::A, 6),
+        0x68 => self.bit_r(RegisterU8::B, 5),
+        0x69 => self.bit_r(RegisterU8::C, 5),
+        0x6A => self.bit_r(RegisterU8::D, 5),
+        0x6B => self.bit_r(RegisterU8::E, 5),
+        0x6C => self.bit_r(RegisterU8::H, 5),
+        0x6D => self.bit_r(RegisterU8::L, 5),
+        0x6E => todo!(),
+        0x6F => self.bit_r(RegisterU8::A, 5),
+
+        // 0x7 codes
+        0x70 => self.bit_r(RegisterU8::B, 6),
+        0x71 => self.bit_r(RegisterU8::C, 6),
+        0x72 => self.bit_r(RegisterU8::D, 6),
+        0x73 => self.bit_r(RegisterU8::E, 6),
+        0x74 => self.bit_r(RegisterU8::H, 6),
+        0x75 => self.bit_r(RegisterU8::L, 6),
+        0x76 => todo!(),
+        0x77 => self.bit_r(RegisterU8::A, 6),
+        0x78 => self.bit_r(RegisterU8::B, 7),
+        0x79 => self.bit_r(RegisterU8::C, 7),
+        0x7A => self.bit_r(RegisterU8::D, 7),
+        0x7B => self.bit_r(RegisterU8::E, 7),
+        0x7C => self.bit_r(RegisterU8::H, 7),
+        0x7D => self.bit_r(RegisterU8::L, 7),
+        0x7E => todo!(),
+        0x7F => self.bit_r(RegisterU8::A, 7),
+
+        _ => panic!("CB Prefixed code not implemented: {:#X}", cb_code)
+    }
+}
 
     // CPU instructions
     // Instructions intepreted from https://gekkio.fi/files/gb-docs/gbctr.pdf
@@ -232,6 +347,13 @@ impl Gameboy {
         self.write_instruction(address, data);
     }
 
+    fn ld_hl_minus_a(&mut self) {
+        let mut address = self.cpu.register.read_u16(RegisterU16::HL);
+        let data = self.cpu.register.read_u8(RegisterU8::A);
+        self.write_instruction(address, data);
+        address -= 1;
+        self.cpu.register.write_u16(RegisterU16::HL, address);
+    }
 
     // 16 bit load instructions
     fn ld_rr_nn(&mut self, r1: RegisterU16) {
@@ -269,7 +391,25 @@ impl Gameboy {
         if result == 0 {
             self.cpu.flags.z = true;
         }
+    }
 
+    // CB Prefix codes
+    fn bit_r(&mut self, r1: RegisterU8, check_bit: u8) {
+
+        // Setting half carry flag and unsetting negative flag as per instruction
+        self.cpu.flags.h = true;
+        self.cpu.flags.n = false;
+
+        let mask: u8 = 1;
+        let mut data = self.cpu.register.read_u8(r1);
+        data = data >> check_bit;
+
+        if (data & mask) == 1 {
+            self.cpu.flags.z = false;
+        }
+        else {
+            self.cpu.flags.z = true;
+        }
     }
 
 
@@ -354,6 +494,26 @@ mod tests {
     }
 
     #[test]
+    fn test_ld_hl_minus_a() {
+        // Create a gameboy for testing purposes
+        let mut gameboy = Gameboy::new();
+        let r1 = RegisterU8::A;
+
+        // Set up gameboy state for test
+        gameboy.cpu.register.write_u8(RegisterU8::H, 0x11);
+        gameboy.cpu.register.write_u8(RegisterU8::L, 0x11);
+        gameboy.cpu.register.write_u8(r1, 0x01);
+
+        // Run test and compare output
+        gameboy.ld_hl_minus_a();
+        let data_in_memory = gameboy.read_instruction(0x1111);
+        let new_hl = gameboy.cpu.register.read_u16(RegisterU16::HL);
+
+        assert_eq!(data_in_memory, 0x01);
+        assert_eq!(new_hl, 0x1110);
+    }
+
+    #[test]
     fn test_ld_rr_nn() {
         // Create a gameboy for testing purposes
         let mut gameboy = Gameboy::new();
@@ -416,4 +576,22 @@ mod tests {
         assert_eq!(new_r1, 0x0F);
         assert_eq!(flag_check, false);
     }
+
+    #[test]
+    fn test_bit_r() {
+        // Create a gameboy for testing purposes
+        let mut gameboy = Gameboy::new();
+        let r1 = RegisterU8::A;
+
+        // Set up gameboy state for test
+        gameboy.cpu.register.write_u8(r1, 0x9F);
+
+        // Run test and compare output
+        gameboy.bit_r(r1, 6);
+
+        assert_eq!(gameboy.cpu.flags.z, true);
+        assert_eq!(gameboy.cpu.flags.n, false);
+        assert_eq!(gameboy.cpu.flags.h, true);
+    }
+
 }
